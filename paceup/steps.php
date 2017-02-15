206036143df997aaf0ba08b27775ac72fabe08da
saztronaut <rmhisrk@ucl.ac.uk>
2017-02-15 12:19:11 +0000

    Add tailored content week 1-6 to the diary page
    Still need to add content to weeks 6+ but will do later

--- paceup/steps.php
+++ paceup/steps.php
@@ -102,12 +102,160 @@
 		document.getElementById("thisHeader").innerHTML= "<h2> Baseline week of your walking plan</h2>";
 		 blurb="<p> Before you start to increase your walking it is important to know how much you are currently doing. The best way to do this is for you to wear the pedometer and record our step counts each day for a full week. There is often quite a difference between weekends and weekdays, so it is important to try and record for a full week. Don't try to increase your walking this week, just do what you normally do, or your targets will be too high and too hard for you to achieve</p>";
 			document.getElementById("thisBlurb").innerHTML= blurb;
+	         thisAside="<p> Before you start to increase your walking it is important to know how much you are currently doing. The best way to do this is for you to wear the pedometer and record our step counts each day for a full week. There is often quite a difference between weekends and weekdays, so it is important to try and record for a full week. Don't try to increase your walking this week, just do what you normally do, or your targets will be too high and too hard for you to achieve</p>";
+			document.getElementById("thisAside").innerHTML= thisAside;
 		 }
 		else {
 			$getweek=weekno;
 			document.getElementById("thisHeader").innerHTML= "<h2> Week "+ $getweek +" of your walking plan</h2>";
-			 blurb="<p> Alternative blurb</p>";
+			switch (weekno){
+				case 1:					
+			          blurb="<p> Your aim for week 1 is to add in an extra 1500 steps on three or more days this \
+week to your baseline steps.</p> <br> \
+<p>One good way to do this is to add in a 15 minute walk.<p>";
+				  thisAside = "<h2> Tips and motivators <h2> <br> \
+<p>Remember walking should be brisk, but not uncomfortable. Fast enough to make \
+ you warm and aware of your breathing, but you should still be able to walk and talk. \
+One way to tell if you are walking at moderate intensity is if you can still talk, but \
+you can’t sing the words to a song!  <br>\
+Make walking part of your daily routine, in order to keep up the changes: <br> \
+Can I fit in an <b>extra</b> walk? <br> \
+Can I <b>increase</b> what I do already? \
+E.g. Get off the bus, tube or train a couple of stops earlier; take a longer route to the \
+shops or library; go for a walk during your lunch break.  <br> \
+If you prefer to, you can get your extra 1500 steps or your extra 15 minutes of \
+moderate intensity physical activity on some days by doing more of other activities \
+you enjoy, such as dancing, playing in the park with your children or grandchildren, \
+or playing badminton or tennis, cycling, or mowing the lawn!</p> <br> <br> \
+<p><i>Walking is man’s best medicine. ~ Hippocrates</i></p> <br> \
+<p><i>Make your feet your friend. ~J.M. Barrie</i></p>";
+				  break;
+				case 2:					
+			          blurb="<p> Your aim for week 2 is to add in an extra 1500 steps on three or more days this \
+week to your baseline steps.</p> <br> \
+<p>One good way to do this is to add in a 15 minute walk.<p>";
+				  thisAside = "<h2>Tips and motivators</h2> \
+<p>Make walking part of your daily routine:<p> <br> \
+<ul type='circle'> \
+    <li>Take the stairs when possible, rather than using a lift or escalator </li> \
+    <li>If you are going somewhere by car, try parking it a bit further away, so that you have to walk a little further </li></ul> \
+ <p> Remember your personal benefits from increasing walking (see page 3 of PACE-UP handbook) </p> <br> \
+<p> What things are important to you in your life that might be improved through increasing your activity and fitness levels?  For example:  health benefits, weight loss, increased energy, improved mood, how you feel about your appearance? </p> <br> \
+<p> What might be the impact and gains of these changes for you?  For example:  </p> \
+<ul type='circle'> \
+  <li> I would be able to get back to playing sport with my friends </li> \
+  <li> It would feel great to be able to wear some new outfits </li> \
+  <li> I could do more with my time </li> \
+  <li> I could keep playing actively with my children or grandchildren </li></ul> \
+<p>If you are falling behind your targets</p> \
+<ul type='circle'> \
+  <li>Don’t give up </li> \
+  <li>If necessary “tread water” , that is, do the same for one week, rather than give up completely </li> \
+  <li>Turn to week 5 of your walking plan for some tips on overcoming obstacles </li></ul> \
+<p> <i>Walking:  the most ancient exercise and still the best modern exercise.  ~Carrie Latet</i></p>";
+			          break;					
+				case 3:					
+			          blurb="<p> Your aim for week 3 is to add in an extra 1500 steps on five or more days this \
+week to your baseline steps.</p> <br> \
+<p>One good way to do this is to add in a 15 minute walk.<p>";
+			          thisAside = "<h2>Keep it up!s</h2> \
+<p>Remember to praise and reward yourself for any success that you have achieved so far, no matter how small it seems!  This will help motivate you to keep going.  </p>\
+<p>Examples:</p> \
+<ul type='circle'> \
+  <li>Spend time noticing any changes in your fitness or appearance</li> \
+  <li>Plan something you enjoy such as meeting a friend or watching a football match</li> \
+  <li>Give yourself some time to relax such as having a bath, a cup of tea or read a paper </li> \
+  <li>Wear those clothes you have been waiting to get in to </li></ul> \
+<p><b>Walking with others makes the activity more enjoyable</b>, so you may be more likely to go for the walk and to keep going. </p> \
+<p> Could you: </p> \
+<ul type='circle'> \
+  <li>Plan some walks with friends and family?  </li> \
+  <li>Plan a walk to an activity you enjoy?</li> \
+  <li>Join a walking group to meet like-minded walkers and make some new friends at the same time? </li> \
+  <li>Walk the dog or a neighbour’s dog? </li></ul> \
+<p> <i>People say that losing weight is no walk in the park.  When I hear that I think, yes, that's the problem.  ~Chris Adams	</i></p>";
+				  break;				
+				case 4:					
+			          blurb="<p> Your aim for week 4 is to add in an extra 1500 steps on five or more days this \
+week to your baseline steps.</p> <br> \
+<p>One good way to do this is to add in a 15 minute walk.<p>";
+				  thisAside = "<h2> Keep motivated!</h2> \
+<p>Well done so far!  Are you remembering to give yourself praise and small rewards for any progress that you make?  </p> \
+<p> Please remember to record your daily step counts.  Seeing the progress you are making in black and white can really help to keep you going. </p> \
+<p>Asking for support and encouragement from family and friends can also be very helpful for keeping up the changes. </p> \
+<p>Notice the changes and benefits. What do I notice and what do others see? Pay attention to any compliments! </p> \
+<p> <i>The best remedy for a short temper is a long walk.  ~Jacqueline Schiff/i></p>";";
+				  break;
+				case 5:					
+			          blurb="<p> Your aim for week 5 is to add in an extra 3000 steps on three or more days this \
+week to your baseline steps.</p> <br> \
+<p>One good way to do this is to add in a 30 minute walk.<p>";
+				  thisAside = "<h2>Now we are moving!</h2> \
+<p>Often increasing your walking means planning ahead and overcoming obstacles </p> \
+<p>Think about some of the obstacles that make you less likely to walk and how you could overcome them: </p> \
+<p><b>Obstacle:</b>  'I don’t have the time to do a 30 minute walk …. I am so pushed for time already' </p> \
+<p><b>Solution:</b>  You don’t have to do your 30 minute walk in one go, you can break it up into walks of 10 or 15 minutes, spread throughout the day. </p> \
+<p><b>Obstacle:</b> 'It’s raining and I’ll be soaked when I arrive for the meeting' </p> \
+<p><b>Solution:</b> Dress for the weather or plan the walk on a different day or in a different place like an indoor shopping centre </p> \
+<p>What are the barriers that make you less likely to walk?   These might include:</p> \
+<ul type='circle'> \
+<li>Places that make it more difficult to be active, e.g. at work in an office </li> \
+<li>Other activities that might get in the way </li> \
+<li>People who make it more difficult to keep up your walking  </li> \
+<li>Thoughts and feelings, e.g. feeling fed up, tired or lethargic </li> \
+<li>Physical symptoms and reactions e.g. back pain or a physical health problem </li> </ul> \
+<p>Think about how you might overcome these obstacles.  List a range of possible solutions and be prepared to experiment to find out what works best. </p> <p><i> Motivation is what gets you started. Habit is what keeps you going. Anonymous</i> </p>";
+				  break;					
+				case 6:					
+			          blurb="<p> Your aim for week 6 is to add in an extra 3000 steps on three or more days this \
+week to your baseline steps.</p> <br> \
+<p>One good way to do this is to add in a 30 minute walk.<p>";
+				  thisAside = "<h2> How to make these changes a permanent part of your life</h2> \
+<p><b>Interest: </b> Are there new walks you could try?  Where might you enjoy walking in your local area?  </p> \
+<ul type='circle'> \
+<li>The local park </li> \
+<li>Countryside or woodlands </li> \
+<li>River Thames or Wandle </li> \
+<li>Tourist attractions in central London </li> \
+<li>Look at the suggested websites GET LINK </li></ul> \
+<p><b>Time/means:</b> What can you not do in order to make time for your walks and make it a priority?</p> \
+<p><b>Gains:  What changes have you noticed so far?</b></p> \
+<p>Take a moment to think about what you have achieved so far.  </p> \
+<p>Has there been any change in your walking pattern and step-count since starting this programme?  Do you feel any different?  Are there changes in your weight, waist size, mood or energy levels?  </p> \
+<p><b>If so, well done!  Give yourself a pat on the back.</b>  Keeping these changes going can lead to real benefits that last over time.</p> \
+<p> <i>I have two doctors, my left leg and my right.  ~G.M. Trevelyan</i></p>";
+				  break;
+				case 7:					
+			          blurb="<p> Alternative blurb</p>";
+				  thisAside = "<p> This motivational message</p>";
+			          break;					
+				case 8:					
+			          blurb="<p> Alternative blurb</p>";
+			          thisAside = "<p> This motivational message</p>";
+				  break;				
+				case 9:					
+			          blurb="<p> Alternative blurb</p>";
+				  thisAside = "<p> This motivational message</p>";
+				  break;
+				case 10:					
+			          blurb="<p> Alternative blurb</p>";
+				  thisAside = "<p> This motivational message</p>";
+				  break;
+				case 11:					
+			          blurb="<p> Alternative blurb</p>";
+			          thisAside = "<p> This motivational message</p>";
+				  break;				
+				case 12:					
+			          blurb="<p> Alternative blurb</p>";
+				  thisAside = "<p> This motivational message</p>";
+				  break;
+				case 13:					
+			          blurb="<p> Alternative blurb</p>";
+				  thisAside = "<p> This motivational message</p>";
+				  break;					
+				    }
 		document.getElementById("thisBlurb").innerHTML= blurb;
+		document.getElementById("thisAside").innerHTML= thisAside;
 		}
 				
 	}
