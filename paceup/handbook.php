73a0057a69171f788ceaae46f9703d62bb59f34a
saztronaut <rmhisrk@ucl.ac.uk>
2017-02-28 10:07:41 +0000

    Add files via upload

---
+++ paceup/handbook.php
@@ -0,0 +1,172 @@
+<div class='jumbotron text-center'>
+<h1>PACE-UP</h1>
+<p> Handbook</p> </div>
+<div class="container-fluid text-center">
+<span id="contents_1"><h3>The benefits of increasing your walking</h3>   </span> 
+<span id="show_1"></span>
+<span id="contents_2"><h3>How much physical activity should you do for health benefits?  </h3>   </span>  
+<span id="show_2"></span>                
+<span id="contents_3"><h3>An introduction to the PACE-UP walking programme</h3>   </span> 
+<span id="show_3"></span>
+<span id="contents_4"><h3>Useful websites for walking</h3>   </span> 
+<span id="show_4"></span>
+<span id="contents_5"><h3>How to keep going when PACE-UP finishes</h3>   </span> 
+<span id="show_5"></span>
+</div>
+
+<div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+        The benefits of increasing your walking</a>
+      </h4>
+    </div>
+    <div id="collapse1" class="panel-collapse collapse in">
+      <div class="panel-body"><div class="row">
+  <div class="col-sm-6"> <h4>Reduce health problems</h4>
+<p>Walking regularly can prevent or improve:</p>
+<li>Heart Disease</li> 
+<li>Stroke</li> 
+<li>Diabetes</li> 
+<li>High blood pressure</li> 
+<li>Osteoporosis</li>  
+<li>Several cancers</li>
+<li>Arthritis</li> 
+<li>back pain</li> </ul>
+<h4>Improve wellbeing</h4></div>
+<div class="col-sm-6"><p>Walking regularly can:</p>
+<ul type='circle'> 
+<li>Relieve stress, anxiety and depression</li> 
+<li>Make you feel happier</li> 
+<li>Increase your energy and fitness</li> 
+<li>Help you to sleep better</li> 
+<li>Improve your quality of life</li>  
+<li>Get you out of the house meeting people</li></ul>
+ <h4>Reduce some of the effects of ageing</h4></div>
+</div><div class="row"><div class="col-sm-6"> 
+<p>Walking regularly can:</p>
+<ul type='circle'> 
+<li>Improve flexibility and balance</li> 
+<li>Protect against developing dementia</li> 
+<li>Maintain mobility and being able to live independently</li> 
+</ul></div> <div class="col-sm-6"> 
+  <h4>Help maintain a healthy weight</h4>
+<p>Walking regularly can:</p>
+<ul type='circle'> 
+<li>Help you to maintain a healthy weight</li> 
+<li>Prevent obesity </li> 
+</ul></div></div>
+<h3> Which of these benefits are most important for you? </h3>
+</div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+        How much physical activity should you do for health benefits?</a>
+      </h4>
+    </div>
+    <div id="collapse2" class="panel-collapse collapse">
+      <div class="panel-body"><p>You should aim to be active daily. Over a week, activity should add up to at least <b>150 minutes</b> (2½ hours) of <b>moderate intensity activity</b> in <b>bouts of 10 minutes</b> or more.</p> 
+<p>One good way to approach this is to do <b>30 minutes activity on at least 5 days a week</b>. This is part of the UK physical activity guidelines for health for adults and older adults (see <a href="www.nhs.uk/livewell/fitness"> activity guidelines on the NHS website </a>) </p>
+<h4> Moderate intensity activity means that you should:</h4>
+<ul type='circle'> 
+<li>Begin to breathe a bit quicker</li> 
+<li>Feel your heart beating a bit faster </li> 
+<li>Feel warmer</li>
+<li>You should still be able to talk whilst you are doing moderate activity.</li>
+</ul>
+
+<h4>How can walking help?</h4>
+<p>Walking at approximately 3 miles (5 km) per hour counts as moderate intensity activity.</p>
+<p>This can be translated into steps per minute and measured with a pedometer.  </p>
+<p>Roughly <b>1500</b> steps in <b>15</b> minutes counts as <b>15 minutes</b> of moderate intensity activity. </p>
+<p>Roughly <b>3000</b> steps in <b>30</b> minutes counts as <b>30 minutes</b> of moderate intensity activity. </p>
+
+<p><b>PACE-UP aims to increase the amount that you walk each week, using a target number of steps measured by a pedometer.</b></p>
+
+<p><b>The UK physical activity guidelines also advise that all adults and older adults should minimise the amount of time that they spend being sedentary (sitting) for extended periods. Taking regular short walks can help you to do this. </p></b>
+</div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+        An introduction to the PACE-UP walking programme</a>
+      </h4>
+    </div>
+    <div id="collapse3" class="panel-collapse collapse">
+      <div class="panel-body">
+	  <p>The plan is to start from where <b>you</b> are currently and to gradually increase the amount you walk over 12 weeks.<p>
+	  <p>ASSUME ADVICE WILL CHANGE HERE re pedometer?</p>
+	  Weeks of PACE-UP walking programme	Target number of steps
+1-2	Add in 1500 steps on 3 or more days per week
+
+3-4	Add in 1500 steps on 5 or more days per week
+
+5-6	Add in 3000 steps on 3 or more days per week
+
+7-12	Add in 3000 steps on 5 or more days per week
+
+
+Remember
+1500 steps equals about 15 minutes of walking &  3000 steps equals about 30 minutes of walking.
+What does this mean for you?
+From the pedometer worn at baseline your average number of daily steps was ....................
+Your 12 week programme will be as follows: 
+Add in extra steps to your baseline average of …………….......…..steps per day. Record your daily step-count on the PACE-UP diary sheets.
+First month add in 1500 steps per day (which is about equal to a 15 minute walk), gradually increasing from 3 to 5 days per week
+Second month add in 3000 steps per day (which is about equal to a 30 minute walk) gradually increasing from 3 to 5 days per week
+Third month is maintenance, keep on adding in 3000 steps per day (about equal to a 30 minute walk) on at least 5 days per week.
+By the end of 12 weeks the aim is for you to be walking an extra 3000 steps most days of the week. If you can do this, your average number of daily steps should have increased to about ............................ steps.
+•	Start low-and-go-slow! This means making any changes in a gradual way.  
+•	Gradually increase both the amount of walking that you do (the number of steps per day) and the intensity (how fast you walk).
+•	Walking at a moderate intensity is safe for most people. If you find it difficult to do 1500 steps in 15 minutes, then you can go more slowly at first and build up gradually.
+•	Wear sturdy shoes or trainers when you walk.  You will be less likely to cause pain in your feet and other joints.
+•	If you feel at all unsteady, try using a walking stick or walking with a friend.
+•	Take some water with you if you are walking a long distance, especially if it is hot weather.
+•	Regular walking is good for arthritis and back pain, but if walking aggravates these problems, you may have done too much too quickly. Rest for a day or so and then try doing slightly less next time.
+•	If walking more or faster gives you chest pain, palpitations or dizziness, or makes you feel faint or fall over, you should stop doing it and tell your GP.
+•	If you are concerned about street safety, identify your local crime hotspots in your area by putting your postcode into http://www.police.uk and check out advice on keeping yourself safe on http://www.suzylamplugh.org/personal-safety/personal-safety-tips/street-safety 
+
+Now you are ready to start!
+Try out your pedometer and check you are happy with the instructions in your PACE-UP walking diary.
+
+Then start recording your steps 	  
+	  </div>
+    </div>
+  </div>
+    <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+        Useful websites for walking</a>
+      </h4>
+    </div>
+    <div id="collapse4" class="panel-collapse collapse">
+      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</div>
+    </div>
+  </div>
+    <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+        How to keep going when PACE-UP finishes</a>
+      </h4>
+    </div>
+    <div id="collapse5" class="panel-collapse collapse">
+      <div class="panel-body">•	Keep the habit of going for a 30 minute walk or doing 30 minutes of other moderate activity to keep up your step-count, most days of the week.
+•	Keep your pedometer and use it sometimes to show you how active you are. It is easy to be very busy without being very active, the pedometer shows you accurately how many steps you are taking.
+•	Remind yourself about what you have achieved by increasing your activity and any positive benefits it has had on your health, weight, mood, sleeping etc. This may motivate you to keep up good habits, or to try again if you feel you have slipped back.
+•	Enlist a friend or family member to walk with you, it is easier to walk regularly and walk further if you have some company.
+•	Try out new walks near you or think about a walking group, the websites listed have lots of ideas for local walks, or your local library will have information.
+.</div>
+    </div>
+  </div>
+</div>
