4f1ca62f5aa97aad82af88b57de07e5bc91c6cdd
saztronaut <rmhisrk@ucl.ac.uk>
2017-03-06 15:36:23 +0000

    cookies

---
+++ paceup/cookies.php
@@ -0,0 +1,11 @@
+<br> <div class = "container">
+<div class="jumbotron"><h2>Cookies</h2>
+</div>
+<h4> What is a cookie? </h4>
+<p>A cookie is a small text file stored in your browser. Cookies are used to help websites work and to provide information to the website owners.</p>
+<h4> Cookies in use </h4>
+<p>PACE-UP Next Steps uses session cookies. The username cookie creates a validated authenticated user session and the form cookie allows users to navigate the website.  </p> 
+<p>Most browsers tend to be set to allow cookies by default, but you can usually change these settings. If you are not sure how to do this please refer to the help function in your browser.<p>
+<p>By using this site you agree that we can place these types of cookies on your device.</p>
+
+<p> At present we are not using Google Analytics, do we want to do this? We need to state this if we are </p>
