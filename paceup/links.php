b35b3b8df4fbec337ac8294a416b83f1cdd72a47
saztronaut <rmhisrk@ucl.ac.uk>
2017-03-20 18:25:02 +0000

    Add more links

--- paceup/links.php
+++ paceup/links.php
@@ -12,14 +12,23 @@
 <a href="www.walkit.com">
 <b>Walkit.com</b></a>
 <p> the urban walking route planner gives you a route map between any two points including your journey time</p>
+  <a href="www.mapmywalk.com"><b>MapMyWalk.com</b></a>
+  <p> Find local walking routes near you </p>
+  <a href="www.walkingforhealth.org.uk"> <b>WalkingForHealth.org.uk </b></a>
+  <p> Find a walk, join a local walking scheme and learn about the benefits of walking for health.</p>
 </div>
 
 <div class="col-sm-5"><h3>For generally keeping active and healthy</h3><br>
 <a href = "www.nhs/livewell/fitness"> <b>NHS Live well</b></a>
 <p>UK physical activity guidelines for adults and older adults, fitness videos and exercises for all age groups</p>
+  <a href="www.nhs.uk/change4life/pages/get-going-every-day.aspx">
+    <b>Change 4 life - get going every day</b></a>
+  <p> Find ways to get active for free </p>
 <a href="https://www.bhf.org.uk/heart-health/preventing-heart-disease/staying-active">
 <b>British Heart Foundation</b></a>
 <p> Ways to be more active and how it will help you</p>
+  <a href= "http://www.nhs.uk/LiveWell/c25k/Pages/couch-to-5k.aspx"> <b> Couch to 5k </b></a>
+  <p>If you've managed to achieve your targets and you want to try jogging, Couch to 5k is a safe way to get started</p>
 </div>
 <div class="col-sm-1"></div>
 </div></div>
